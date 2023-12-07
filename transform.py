62919cad0887d3497cae2a0efd920379439aee1b
sarahshh <sarah.sahli.2@gmail.com>
2023-12-07 23:21:01 +0100

    modification du fichier transform.py

--- transform.py
+++ transform.py
@@ -2,123 +2,131 @@
 
 import pandas as pd
 import numpy as np
-import datetime
-from dateutil import parser
-import math
-import boto3
-import io
 import os
 import dotenv
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
+# récupération des tarifs confidentiels dans le .env
+os.environ.clear()
+load_dotenv(find_dotenv(".env"))
+extra_salaire = float(os.environ.get("extra_salaire"))
+extra_salaire_urgence = float(os.environ.get("extra_salaire_urgence"))
 
-#  -- transformation de la table missions --
-# ajout de la colonne Total_HT
-# ajout de la colonne Total_TTC
-# ajout de la colonne Bénéfice
-# ajout de la colonne salaire_extra
-# transformation de la colonne date en deux colonnee date_debut et date_fin
-# ajout d'une colonne durée_mission en heures
-# ajout d'une colonne heure_sup
+# import des fichiers anonymisés
+extras = pd.read_csv('fichiers-anonymes/extras_anonymes.csv')
+missions = pd.read_csv('fichiers-anonymes/missions_anonymes.csv')
 
-df = pd.read_csv('/home/gregoirek/Documents/JEDHA/2_Fullstack/x_projet_final/extras_anonymes.csv')
-df_missions = pd.read_csv('/home/gregoirek/Documents/JEDHA/2_Fullstack/x_projet_final/missions_anonymes.csv')
-
-colonne_extra = ['nom', 'logiciel', 'Disponibilités', 'dispos', 'vacances', 'statuts','Date de création']
-colonne_missions = ['hôtel', 'extra', 'date', 'statuts', 'tarif urgence', 'tarif horaire','facture', 'règlement', 'Texte']
-
+#  -- colonnes finales de la table missions --
+# ['hôtel', 'extra', 'tarif urgence', 'tarif horaire', 'facture', 'règlement', 'date_debut', 'date_fin', 'duree_mission', 'urgent',
+#      'annulation', 'heures_supp', 'Total_HT', 'Total_TTC', 'extra_salaire', 'bénéfice']
 
 def transform_missions(missions):
-    # -- séparation de la colonne date en deux colonnes date_debut et date_fin --
-    # suppression de (UTC) dans la colonne date
-    df_missions['date'] = df_missions['date'].str.replace(' (UTC)', '')
-    df_missions['date'] = df_missions['date'].str.replace(' (UTC+3)', '')
-    # je vérifie qu'il n'y a plus de (UTC) dans la colonne date
-    print(df_missions['date'].str.contains('UTC+3').value_counts(), df_missions['date'].str.contains('UTC').value_counts())
-    print("Suppression de (UTC) et (UTC+3) dans la colonne date terminée.")
+    # -- Transformation de la colonne date en date_debut et date_fin --
 
-    # je sépare la colonne date en cinq colonnes date_debut, heure_début, colonne contenant " → " , date_fin et heure_fin
-    df_missions[['date_debut', 'heure_debut', 'vide', 'date_fin', 'heure_fin']] = df_missions['date'].str.split(' ', expand=True)
-    
-    # si heure_fin est vide, heure_fin = date_fin 
-    df_missions['heure_fin'] = np.where(df_missions['heure_fin'].isnull(), 
-                                        df_missions['date_fin'], df_missions['heure_fin'])
-    # si heure_fin == date_fin, date_fin = date_debut
-    df_missions['date_fin'] = np.where(df_missions['heure_fin'] == df_missions['date_fin'], 
-                                       df_missions['date_debut'], df_missions['date_fin'])
-    # je supprime la colonne vide
-    df_missions.drop('vide', axis=1, inplace=True)
-    # je fusionne date_debut et heure_debut en une seule colonne date_debut
-    df_missions['date_debut'] = df_missions['date_debut'] + " " + df_missions['heure_debut']
-    # je fusionne date_fin et heure_fin en une seule colonne date_fin
-    df_missions['date_fin'] = df_missions['date_fin'] + " " + df_missions['heure_fin']
-    # je supprime les colonnes heure_debut et heure_fin
-    df_missions.drop(['heure_debut', 'heure_fin'], axis=1, inplace=True)
-    # je transforme les colonnes date_debut et date_fin en datetime
-    df_missions['date_debut'] = pd.to_datetime(df_missions['date_debut'], format='%d/%m/%Y %H:%M')
-    df_missions['date_fin'] = pd.to_datetime(df_missions['date_fin'], format='%d/%m/%Y %H:%M')
+    missions.dropna(subset=['hôtel', 'extra'], inplace=True) # on retire les lignes vides pour l'hôtel ou l'extra 
+    missions.reset_index(drop=True, inplace=True) # reindexation
 
+    # on retire (UTC) et (UTC+3) dans la colonne date
+    missions['date'] = missions['date'].str.replace('(UTC)', '') 
+    missions['date'] = missions['date'].str.replace('(UTC+3)', '')
+
+    # On separe la colonne date en date_debut et date_fin
+    missions['date_debut'] = missions['date'].str.split(' → ').str[0]
+    missions['date_fin'] = missions['date'].str.split(' → ').str[1]
+
+    # on retire les espaces qui peuvent poser problème au début et à la fin des colonnes
+    missions['date_debut'] = missions['date_debut'].str.strip()
+    missions['date_fin'] = missions['date_fin'].str.strip()
+
+    nb_lignes = missions.shape[0]
+
+    # la colonne date_fin ne contient pas toujours de date, parfois il y a uniquement l'heure de fin indiquée
+    for i in range(nb_lignes):
+        if '/' in missions.loc[i, 'date_fin']: # si la date_fin contient déjà une date, on la conserve
+            missions.loc[i, 'date_fin'] = missions.loc[i, 'date_fin'] 
+        else: # sinon, on ajoute la date_debut à l'heure dans la colonne date_fin
+            missions.loc[i, 'date_fin'] = missions.loc[i,'date_debut'].split(' ')[0] + ' ' + missions.loc[i, 'date_fin']
+
+    # conversion des colonnes date_debut et date_fin en datetime 
+    missions['date_debut'] = pd.to_datetime(missions['date_debut'], format='%d/%m/%Y %H:%M')
+    missions['date_fin'] = pd.to_datetime(missions['date_fin'], format='%d/%m/%Y %H:%M')
     print("Transformation de la colonne date terminée.")
-    # -- fin de la transformation de la colonne date --
 
-    # -- ajout de la colonne durée_mission en heures --
-    # je crée une colonne durée_mission qui contient la différence entre date_debut et date_fin
-    df_missions['durée_mission'] = df_missions['date_fin'] - df_missions['date_debut']
-    # j'obtiens le résultat sous la forme 0 days 12:00:00
-    # je conserve uniquement 12:00:00 (position str[7])
-    df_missions['durée_mission'] = df_missions['durée_mission'].astype(str).str[7:]
-    df_missions['durée_mission'] = pd.to_datetime(df_missions['durée_mission'], format='%H:%M:%S')
-    df_missions['durée_mission'] = df_missions['durée_mission'].dt.hour
+    # -- fin de la transformation de la colonne date --
 
-    print("Ajout de la colonne durée_mission terminée.")
+    # -- Ajout d'une colonne duree_mission en heures -- 
+    missions['duree_mission'] = (missions['date_fin'] - missions['date_debut']).dt.total_seconds()/3600
+    print("Ajout de la colonne duree_mission terminée.")
  
     # -- fin de l'ajout de la colonne durée_mission --
 
+    # -- Transformation de la colonne 'statuts' en colonnes 'urgent' et 'annulation'
+    missions['urgent'] = missions['statuts'].apply(lambda x: 'urgente' in x if pd.notna(x) else False )
+
+    missions['annulation'] = missions['statuts'].apply(lambda x: 'extra' if x == 'annulé extra' 
+                                                    else 'hôtel' if x == 'annulé hôtel'
+                                                    else np.nan)
+    
+    missions.drop('statuts', axis=1, inplace=True) # suppression de la colonne statuts
+
+    # -- Ajout d'une colonne heures_supp et suppression de la colonne Texte --
+    replace_supp = {'30 min supp': 0.5, '1h30 supp': 1.5, '3h supp': 3, # dictionnaire qui remplace le texte par la duree en heures 
+                '2h supp': 2, '1h supp': 1, '2h30 heures supp': 2.5, 
+                '30 minutes heures supp': 0.5}
+    
+    missions['heures_supp'] = missions['Texte'].replace(replace_supp)
+
+    missions.drop('Texte', axis=1) # suppression de la colonne Texte
+    print("Ajout de la colonne heures_supp terminée.")
+
+    # -- fin de l'ajout de la colonne heures_supp --
 
     # -- ajout de la colonne TOTAL_HT --
-    # je crée une colonne TOTAL_HT qui contient le produit de la colonne durée_mission et de la colonne tarif horaire
-    # je rajoute une condition qui vérifie si la colonne tarif urgence est vide ou non
-    # j'utilise la fonction np.where qui permet de faire une condition
-    df_missions['Total_HT'] = np.where(df_missions['tarif urgence'].isnull(), 
-                                       df_missions['durée_mission'] * df_missions['tarif horaire'], 
-                                       df_missions['durée_mission'] * df_missions['tarif urgence'])
-    
-    print("Ajout de la colonne TOTAL_HT terminée.")
+    # 0€ si annulation, duree_mission * tarif horaire si non urgent, duree_mission * tarif urgence si urgent
+    missions['Total_HT'] = missions.apply(lambda row: 0 if pd.notna(row['annulation'])
+                                    else row['duree_mission'] * row['tarif horaire'] if not row['urgent']
+                                    else row['duree_mission'] * row['tarif urgence'], axis=1)
 
+    # ajout des heures supp en tarif urgence 
+    missions['Total_HT'] += missions['heures_supp'].apply(lambda x: x * 25 if pd.notna(x) else 0)    
+
+    print("Ajout de la colonne TOTAL_HT terminée.")
+                            
     # -- fin de l'ajout de la colonne TOTAL_HT --
 
     # -- ajout de la colonne TOTAL_TTC --
-    # je crée une colonne TOTAL_TTC qui est un produit de df_missions['Total_HT'] et de 1.2
-    df_missions['Total_TTC'] = df_missions['Total_HT'] * 1.2
+    # je crée une colonne TOTAL_TTC qui est un produit de extras_missions['Total_HT'] et de 1.2
+    missions['Total_TTC'] = missions['Total_HT'] * 1.2
 
     print("Ajout de la colonne TOTAL_TTC terminée.")
 
     # -- fin de l'ajout de la colonne TOTAL_TTC --
 
     # -- ajout de la colonne extra_salaire --
-    # les extra sont payé 15€ de l'heure sauf en cas d'urgence ou ils sont payé 20€ de l'heure
-    df_missions['extra_salaire'] = np.where(df_missions['tarif urgence'].isnull(), 
-                                       df_missions['durée_mission'] * 15, 
-                                       df_missions['durée_mission'] * 20)  
+    # 0€ si annulation, duree_mission * extra_salaire si non urgent, duree_mission * extra_salaire_urgence si urgent
+    missions['extra_salaire'] = missions.apply(lambda row: 0 if pd.notna(row['annulation'])
+                                    else row['duree_mission'] * extra_salaire if not row['urgent']
+                                    else row['duree_mission'] * extra_salaire_urgence, axis=1)
+
+    # ajout des heures supp en salaire urgence 
+    missions['extra_salaire'] += missions['heures_supp'].apply(lambda x: x * extra_salaire_urgence if pd.notna(x) else 0)   
     print("Ajout de la colonne extra_salaire terminée.")
     # -- fin de l'ajout de la colonne extra_salaire --
 
     # -- ajout de la colonne bénéfice --
     # la colonne bénéfice correspond au total ht moins le salaire de l'extra
-    df_missions['bénéfice'] = df_missions['Total_HT'] - df_missions['extra_salaire']
+    missions['bénéfice'] = missions['Total_HT'] - missions['extra_salaire']
     print("Ajout de la colonne bénéfice terminée.")
     # -- fin de l'ajout de la colonne bénéfice --
 
     print("Toutes les transformations sont terminées.")
     print("######################")
 
-
-
  
 
    
 
-def transform_extra(extra):
+def transform_extra(extras):
     liste_mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet',
               'août', 'septembre', 'octobre', 'novembre', 'décembre']
     # -- Transformation de la colonne vacances --
@@ -127,38 +135,33 @@ def transform_extra(extra):
         # i : index de 0 à 11
         # v mois de janvier à décembre
         # on remplace les mois par leur numéro on précise i+1 car l'index commence à 0
-        df['vacances'] = df['vacances'].str.replace(v, str(i+1))
+        extras['vacances'] = extras['vacances'].str.replace(v, str(i+1))
 
     # je sépare la colonne vacances en deux colonnes vacances_debut et vacances_fin
-    df['vacance_debut'] = df['vacances'].str.split(' → ').str[0]
-    df['vacance_fin'] = df['vacances'].str.split(' → ').str[1]
+    extras['vacance_debut'] = extras['vacances'].str.split(' → ').str[0]
+    extras['vacance_fin'] = extras['vacances'].str.split(' → ').str[1]
 
     # les dates sont sous la forme 01 01 2020 00:00 je les transforme en datetime
-    df['vacance_debut'] = pd.to_datetime(df['vacance_debut'], format='%d %m %Y %H:%M') 
-    df['vacance_fin'] = pd.to_datetime(df['vacance_fin'], format='%d %m %Y %H:%M')
+    extras['vacance_debut'] = pd.to_datetime(extras['vacance_debut'], format='%d %m %Y %H:%M') 
+    extras['vacance_fin'] = pd.to_datetime(extras['vacance_fin'], format='%d %m %Y %H:%M')
     print("Transformation de la colonne vacances terminée.")
     # -- fin de la transformation de la colonne vacances --
 
     # -- Transformation de la colonne Disponibilités --
     # je fusionne les colonnes disponibilités et dispos
 
-    df['Dispobilités'] = df['Disponibilités'] + " , " + df['dispos']
+    extras['Dispobilités'] = extras['Disponibilités'] + " , " + extras['dispos']
     # je supprime les colonnes dispos et Disponibilités
-    df.drop(['dispos', 'Disponibilités'], axis=1, inplace=True)
+    extras.drop(['dispos', 'Disponibilités'], axis=1, inplace=True)
     print("Transformation de la colonne Disponibilités terminée.")
     # -- fin de la transformation de la colonne Disponibilités --
 
     print("Toutes les transformations sont terminées.")
     print("######################")
 
+transform_missions(missions)
 
-
-
-
-transform_missions(missions = df_missions)
-
-transform_extra(extra = df)
-
+transform_extra(extras)
         
 
 
