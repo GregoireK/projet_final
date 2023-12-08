c8578c37075ed58deb2a4ac28581d19969d9cd01
GregoireK <gr3g01r3k@protonmail.com>
2023-12-08 11:39:38 +0100

    finalisation de la connexion avec le S3 bucket et fetch des csv avant envoie vers le transform.py

--- connexion.py
+++ connexion.py
@@ -4,9 +4,11 @@ import boto3
 import psycopg2
 import pandas as pd
 import sys
-import io
 import os
-import dotenv
+from io import BytesIO
+from io import StringIO
+import botocore
+from botocore.exceptions import NoCredentialsError
 from dotenv import load_dotenv, find_dotenv
 
 # on utilise un env pour masquer les identifiants de connexion.
@@ -18,33 +20,60 @@ from dotenv import load_dotenv, find_dotenv
 # la fonction renverra un dataframe pandas pour chaque csv.
 
 def session_boto3():
-    # avec load_dotenv je charge un .env que j etrouve avec la fonction find_dotenv
+    os.environ.clear()
     load_dotenv(find_dotenv("/home/gregoirek/Documents/JEDHA/2_Fullstack/x_projet_final/dotenv/.env"))
+
+    aws_access_key_id=os.environ.get("AWS_S3_ACCES_KEY_ID")
+    aws_secret_access_key=os.environ.get("AWS_S3_SECRET_ACCESS_KEY")
+    region_name = 'eu-west-3'
+
     # je créer une session avec boto3 dans laquel je passe mes key (du fihcier .env)
     # la .session() de boto" meconnecte à AWS
-    session = boto3.Session(aws_access_key_id=os.environ.get("AWS_S3_ACCESS_KEY_ID"), 
-                            aws_secret_access_key=os.environ.get("AWS_S3_SECRET_ACCESS_KEY"),
-                            region_name='eu-west-3')   
+    session = boto3.Session(aws_access_key_id=aws_access_key_id, 
+                            aws_secret_access_key=aws_secret_access_key,
+                            region_name=region_name)
+    
     return session
 
-def connexion_s3(session_boto3):
-    os.environ.clear()
-
- 
-     # avec load_dotenv je charge un .env que j etrouve avec la fonction find_dotenv
-    load_dotenv(find_dotenv("/home/gregoirek/Documents/JEDHA/2_Fullstack/x_projet_final/dotenv/.env"))
+def connexion_s3(session):
 
     
     # la variable session contient la session de connexion 
     # avec .resosurce() je précise que je veux acceder à s3
-    s3 = session_boto3.resource("s3")
+    s3 = session.client("s3")
 
-    # se connecter à un bucket avec la fonction .Bucket()
-    bucket = s3.Bucket("bucketprojetfinal")
+    bucket = "bucketprojetfinal"
 
-    return "Connexion réussie au bucket S3."
+    key_missions = "missions_anonymes.csv"
+    key_hotel = "hotel_anonymes.csv"
+    key_extras = "extras_anonymes.csv"
 
-def connexion_rds(session_boto3):
+    try:
+       # on tuilise la méthode get_object de boto3 pour récupérer les csv
+       # on précise le bucket et la key du csv
+       # on utilise la méthode read() pour lire le csv
+        missions = s3.get_object(Bucket=bucket, Key=key_missions)
+        missions_csv_content = missions['Body'].read().decode('utf-8')
+
+        hotels = s3.get_object(Bucket=bucket, Key=key_hotel)
+        hotels_csv_content = hotels['Body'].read().decode('utf-8')
+
+        extras = s3.get_object(Bucket=bucket, Key=key_extras)
+        extra_csv_content = extras['Body'].read().decode('utf-8')
+
+        # on utilise StringIO pour lire le csv avec pandas
+        df_missions = pd.read_csv(StringIO(missions_csv_content))
+        df_hotel = pd.read_csv(StringIO(hotels_csv_content))
+        df_extras = pd.read_csv(StringIO(extra_csv_content))
+
+        return df_missions, df_hotel, df_extras
+    
+    except Exception as e:
+        print(f"Error reading CSV from S3: {e}")
+        return None
+
+
+def connexion_rds(session):
     os.environ.clear()
     ENDPOINT= "database-1.cxpuckismlkx.eu-west-3.rds.amazonaws.com"
     PORT = "5432"
@@ -58,7 +87,7 @@ def connexion_rds(session_boto3):
     # je créer une session avec boto3 dans laquel je passe mes key (du fihcier .env)
     # la .session() de boto" meconnecte à AWS
 
-    rds_client = session_boto3.client('rds')
+    rds_client = session.client('rds')
     #token = rds_client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
     #print(token, type(token))
     try:
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import pandas as pd
 import io
 import os
@@ -14,15 +15,13 @@ def main():
     # fonction de création de la session boto3
     # cette session permettra la connexion avec le S3 et le rds
     session = session_boto3()
-    connect_s3 = connexion_s3(session)  
-    print(connect_s3)
-
-    connect_rds = connexion_rds(session)
-
-    print(connect_rds)
-
-
-    #transform_missions()
+    
+    # fonction de connexion au S3 bucket qui renvoie trois dataframe pandas df_mission, df_hotel, df_extras
+    s3 = connexion_s3(session)   
+    df_mission, df_hotel, df_extras = s3[0], s3[1], s3[2]
+    # on lance les focntion de transformation auquel on passe en argument le datafram corresondant
+    #transform_missions(df_mission)
+    #transform_extra(df_extras)
 
 
     
